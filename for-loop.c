fe88d288dceb9ea09f53ba934696d6c65f03c3a0
deepaksyadav <deepaksyadav@users.noreply.github.com>
2021-10-19 23:18:36 +0530

    Create for-loop.c

---
+++ for-loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main () {
+
+   int counter;
+	
+   /* for loop execution */
+   for( counter = 10; counter < 20; counter = counter + 1 ){
+      printf("value of counter: %d\n", counter);
+   }
+ 
+   return 0;
+}
