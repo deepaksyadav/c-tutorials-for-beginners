e20955801986a5843543aed4a36e3fa4f90be9aa
deepaksyadav <deepaksyadav@users.noreply.github.com>
2021-10-19 23:26:11 +0530

    Create if-statement-example.c

---
+++ if-statement-example.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main () {
+
+   /* local variable definition */
+   int intVar = 10;
+ 
+   /* check the boolean condition using if statement */
+	
+   if( intVar < 20 ) {
+      /* if condition is true then print the following */
+      printf("intVar is less than 20\n" );
+   }
+   
+   printf("value of intVar is : %d\n", intVar);
+ 
+   return 0;
+}
