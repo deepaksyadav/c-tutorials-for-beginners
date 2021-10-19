4907789655a6913a4f9ad1fa56197d47f5bf66cf
deepaksyadav <deepaksyadav@users.noreply.github.com>
2021-10-19 23:16:55 +0530

    Create while-loop.c

---
+++ while-loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main () {
+
+   /* local variable definition */
+   int intVar = 10;
+
+   /* while loop execution */
+   while( a < 20 ) {
+      printf("value of intVar: %d\n", intVar);
+      intVar++;
+   }
+ 
+   return 0;
+}
